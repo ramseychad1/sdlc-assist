66ffe8bb997109c5d308edb22d9ba3f75aef18c7
Chad Ramsey <ramseychad1@gmail.com>
2026-02-15 12:05:34 -0500

    Updated google call to asyncß

--- backend/src/main/java/com/sdlcassist/SdlcAssistApplication.java
+++ backend/src/main/java/com/sdlcassist/SdlcAssistApplication.java
@@ -2,8 +2,10 @@ package com.sdlcassist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SdlcAssistApplication {
 
     public static void main(String[] args) {
--- backend/src/main/java/com/sdlcassist/service/EmailService.java
+++ backend/src/main/java/com/sdlcassist/service/EmailService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Async
     public void sendInviteEmail(String toEmail, String displayName, String username, String password) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("ramseychad1@gmail.com");
